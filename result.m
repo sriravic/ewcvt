82356f760d0134812ef88b05a75fd6a784745685
Srinath Ravichandran <srinath.pop@gmail.com>
2014-05-01 10:52:10 +0530

    Create result.m

---
+++ result.m
@@ -0,0 +1,23 @@
+function result(cluster)
+
+size =128;
+output=uint8(zeros(size,size,3));
+for i=1:size
+    for j=1:size;
+    if(cluster(i,j,1)==1)
+    output(i,j,1)=255;
+    end
+    if(cluster(i,j,2)==1)
+    output(i,j,2)=255;
+    end
+%     if(cluster(i,j,3)==1)
+%     output(i,j,3)=255;
+%     end
+    end
+end
+
+
+figure,imshow(output);
+
+imwrite(output,'night_cvt.jpg','jpg');
+%imwrite(I,'abhi1_equal.jpg','jpg');
